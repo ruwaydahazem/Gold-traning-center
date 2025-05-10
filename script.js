a59583e6c24f5f9afb6e474e90f6b1af23d2cfc7
ruwaydahazem <hazmrwydt6@gmail.com>
2025-05-10 20:16:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showTrainers() {
+    document.getElementById("output").innerHTML = "<li>أ. محمد عبد الله</li><li>أ. ليلى حسين</li><li>أ. أحمد يوسف</li>";
+}
+function showCourses() {
+    document.getElementById("output").innerHTML = "<li>تصميم باستخدام الفوتوشوب</li><li>اللغة الإنجليزية</li><li>الترجمة الاحترافية</li>";
+}
+function showTimes() {
+    document.getElementById("output").innerHTML = "<li>السبت - الإثنين: 10 صباحاً</li><li>الثلاثاء - الخميس: 4 مساءً</li>";
+}
+function showFees() {
+    document.getElementById("output").innerHTML = "<li>تصميم: 150$</li><li>اللغة الإنجليزية: 120$</li><li>الترجمة: 100$</li>";
+}
